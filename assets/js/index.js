6c15efc89769e9cdabd585523501b4c6c3e8e729
RiccardoCacio <riccardocacio@users.noreply.github.com>
2023-02-05 12:54:13 +0100

    .
    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,35 @@
 
 window.onload = function () {
 
-    var mybuttonpiu = document.getElementById('buttonPlus');
-    var mybuttonmeno = document.getElementById('buttonLess');
-    var mybuttonreset = document.getElementById('buttonReset');
     var myrisultato = document.getElementById('result');
-
+    var container = document.getElementById('container');    
     var counter = 0;
+    
+    //buttonPiu
+    var mybuttonpiu = document.createElement('button')
+    mybuttonpiu.textContent='+'
+    container.appendChild(mybuttonpiu);
+    mybuttonpiu.setAttribute('id','buttonPlus')
+
+    //buttonMeno
+    var mybuttonmeno = document.createElement('button')
+    mybuttonmeno.textContent='-'
+    container.appendChild(mybuttonmeno);
+    mybuttonmeno.setAttribute('id','buttonLess')
+
+     //buttonReset
+    var mybuttonreset = document.createElement('button')
+    mybuttonreset.textContent='Reset'
+    container.appendChild(mybuttonreset);
+    mybuttonreset.setAttribute('id','buttonReset')
+
+    //buttonReset
+    var myrisultato = document.createElement('p')
+    myrisultato.textContent='0'
+    container.appendChild(myrisultato);
+    myrisultato.setAttribute('id','result')
+
+    //counter
 
     mybuttonpiu.addEventListener("click", () => {
         counter++
@@ -25,5 +48,6 @@ window.onload = function () {
         myrisultato.innerHTML = counter;
         console.log(counter)
     })
+    
 
 }
